c98efcfaffc1abbbd509a38b3fd6151784b0ddba
Yulia <yulia.tarima@gmail.com>
2019-10-03 14:10:09 -0700

    Annotation Sequence Additional Info

--- src/test/java/com/test/Testng1AnnotationSequence.java
+++ src/test/java/com/test/Testng1AnnotationSequence.java
@@ -18,66 +18,66 @@ public class Testng1AnnotationSequence {
 	public void setUp() {
 		System.out.println("+++++++++++++++++++++++++++++++++++\n\n"
 				+ "Runs only once for the whole file\n"
-				+ "#1 in Sequence @BeforeSuite: Setup system property for chrome\n");
+				+ "#1 in Sequence, once, @BeforeSuite: Setup system property for chrome\n");
 	}
 	
 	@BeforeTest 
 	public void launchBrowser() {
-		System.out.println("#2 in Sequence @BeforeTest: Launching chrome browser");
+		System.out.println("#2 in Sequence, once, @BeforeTest: Launching chrome browser");
 	}
 	
 	@BeforeClass 
 	public void logIn() {
-		System.out.println("#3 in Sequence @BeforeClass: Logging into app\n\n"
+		System.out.println("#3 in Sequence, once, @BeforeClass: Logging into app\n\n"
 				+ "++++++++++++++++++++++++++++++++++++++++++++\n");
 	}
 
 	@BeforeMethod 
 	public void enterURL() {
 		System.out.println("This block runs for each test case\n"
-				+ "#4 in Sequence @BeforeMethod: Entering URL\n");
+				+ "#4 in Sequence, each TC, @BeforeMethod: Entering URL\n");
 	}
 	
 	//@Test - Test cases
 	
 	@Test 
 	public void googleTitleTest() {
-		System.out.println("#5 in Sequence @Test: Google Title Test (Test Case 1)");
+		System.out.println("#5 in Sequence, each TC, @Test: Google Title Test (Test Case 1)");
 	}
 	
 	@Test
 	public void searchTest(){
-		System.out.println("#5 in Sequence @Test: Search Test (Test Case 2)");
+		System.out.println("#5 in Sequence, each TC, @Test: Search Test (Test Case 2)");
 	}
 	
 	@Test
 	public void googleLogoTest() {
-		System.out.println("#5 in Sequence @Test: Google Logo Test (Test Case 3)");
+		System.out.println("#5 in Sequence, each TC, @Test: Google Logo Test (Test Case 3)");
 	}
 	
 	//@After- Post-conditions
 	
 	@AfterMethod 
 	public void logOut() {
-		System.out.println("#6 in Sequence @AfterMethod: Logging out of app\n\n"
+		System.out.println("#6 in Sequence, each TC, @AfterMethod: Logging out of app\n\n"
 				+ "+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 	}
 	
 	@AfterClass 
 	public void closeBrowser() {
 		System.out.println("Runs after all of test cases are over per file\n"
-				+ "#7 in Sequence @AfterClass: Closing Browser\n");
+				+ "#7 in Sequence, once, @AfterClass: Closing Browser\n");
 	}
 	
 	@AfterTest 
 	public void deleteAllCookies() {
-		System.out.println("#8 in Sequence @AfterTest: Deleting all cookies\n\n"
+		System.out.println("#8 in Sequence, once, @AfterTest: Deleting all cookies\n\n"
 				+ "++++++++++++++++++++++++++++++++++++++++++++++\n");
 	}
 	
 	@AfterSuite
 	public void generateTestReport() {
-		System.out.println("#9 in Sequence @AfterSuite: Generating Test Report");
+		System.out.println("#9 in Sequence, once, @AfterSuite: Generating Test Report");
 	}
 
 }
