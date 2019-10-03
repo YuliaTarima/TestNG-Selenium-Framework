1db0901cb86c35f8f3aba47f3e4e259e1d89c282
Yulia <yulia.tarima@gmail.com>
2019-10-03 15:59:43 -0700

    Added Google Test

---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,11 @@
+package com.qa.util;
+
+import com.qa.base.TestBase;
+
+public class TestUtil extends TestBase {
+
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 20;
+
+	
+}
