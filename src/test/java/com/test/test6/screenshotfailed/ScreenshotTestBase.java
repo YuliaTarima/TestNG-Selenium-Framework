1ff6112feb74afa83890f666e1452bd482ab5785
Yulia <yulia.tarima@gmail.com>
2019-10-21 20:57:33 -0700

    added screenshots for failed cases at src\test\java\com\test\test6

---
+++ src/test/java/com/test/test6/screenshotfailed/ScreenshotTestBase.java
@@ -0,0 +1,82 @@
+package com.test.test6.screenshotfailed;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+
+import com.util.TestUtil;
+
+public class ScreenshotTestBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public void initialization() {
+		// set up properties file
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/java/com/config/googleConfig.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// set up browser
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("FF")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+		Reporter.log("Application is set up successfully");
+	}
+
+	public void screenshotFailedTest(String testMethodName) {
+		String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		// creates FailedTestsScreenshots folder under src folder after execution
+		//String filePath = System.getProperty("user.dir")+"/FailedTestsScreenshots/failshot_"+this.getClass().getName()+"_"+date+".png";
+		String filePath = System.getProperty("user.dir")+"/FailedTestsScreenshots/"+testMethodName+"_"+date+".png";
+		File destination = new File(filePath);
+
+		try {
+			FileUtils.copyFile(source, destination);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void finishUp() {
+		driver.quit();
+		Reporter.log("Browser Session End");
+	}
+}
